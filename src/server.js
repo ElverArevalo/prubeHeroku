cf7e1c6de9eb8274e861428a22ad616223fbd714
ElverArevalo <elver.arevalo@gmail.com>
2020-06-21 20:20:21 -0500

    =prubeaheroku13

--- src/server.js
+++ src/server.js
@@ -1,16 +1,11 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + 'prueba-heroku'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'prueba-heroku/server.js'));
+app.use(express.static(__dirname+'/dist/pruebaHeroku'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/pruebaHeroku/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
